c0b8bb9e61e2904e080c6cec74f4a6617a69fb60
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2023-08-02 19:33:56 +0400

    add delete feature

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,18 @@ function App() {
       return prevTodos.concat(newTodo);
     });
   };
+
+  const deleteTodoHandler = (todoID: number) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoID);
+    });
+  };
+
   return (
     <div className="app">
       <h1>My Todos</h1>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todo items={todos} />
+      <Todo items={todos} onDelete={deleteTodoHandler} />
     </div>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,11 +3,17 @@ import todo from "../model/todo";
 import TodoItem from "./TodoItem";
 import classes from "./Todo.module.css";
 
-const Todo: React.FC<{ items: todo[] }> = (props) => {
+const Todo: React.FC<{ items: todo[]; onDelete: (id: number) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.title} />
+        <TodoItem
+          key={item.id}
+          text={item.title}
+          onDelete={() => props.onDelete(item.id)}
+        />
       ))}
     </ul>
   );
